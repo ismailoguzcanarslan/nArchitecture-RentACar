ab54f91f35aded0eb49e9e0d01a80c439262288a
İsmail Oğuzcan Arslan <ismailoguzcanarslan@gmail.com>
2022-09-02 18:50:48 +0300

    Kodlama.io.Devs application design and first migration

---
+++ src/demoProjects/Kodlama.io.Devs/Kodlama.io.Devs.Domain/Entities/ProgrammingLanguage.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Domain.Entities
+{
+    public class ProgrammingLanguage : Entity
+    {
+        public string Name { get; set; }
+
+        public ProgrammingLanguage()
+        {
+
+        }
+
+        public ProgrammingLanguage(int id, string name) : this()
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
