adc0f5db649b4247d9eeae06d94aeb548c0b7bf5
İsmail Oğuzcan Arslan <ismailoguzcanarslan@gmail.com>
2022-09-03 02:19:29 +0300

    Validation added for "Add" endpoint

---
+++ src/demoProjects/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    internal class CreateProgrammingLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        public CreateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+        }
+    }
+}
